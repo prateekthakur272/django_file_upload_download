839925dbbe1ee2eb2e2aea8dbba5cab8aba721d4
Prateek Thakur <prateekthakur272@gmail.com>
2023-10-22 12:41:27 +0530

    adds file model

--- files/models.py
+++ files/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class File(models.Model):
+    class Meta():
+        verbose_name = 'File'
+        verbose_name_plural = 'Files'
+    
+    file = models.FileField(upload_to='files')
+    name = models.CharField(max_length=200)
